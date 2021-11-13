bbf054479268aae55739d9974f627a005af08cd6
unknown <guilhermehenrique.oliv3@gmail.com>
2021-11-13 08:26:48 -0300

    Project created

---
+++ backend/src/main/java/com/firedev/firedev/FiredevApplication.java
@@ -0,0 +1,13 @@
+package com.firedev.firedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiredevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiredevApplication.class, args);
+	}
+
+}
